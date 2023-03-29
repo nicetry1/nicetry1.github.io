f199e7fcea7e7b56ebd3d5048184fbc25f45814d
nicetry1 <83890805+nicetry1@users.noreply.github.com>
2023-03-28 23:17:59 -0400

    trying modal view

--- scripts/script.js
+++ scripts/script.js
@@ -26,3 +26,41 @@ function handleFormSubmit(event){
     }
   );
 };
+
+let firstModal = document.getElementById("firstModal");
+let secondModal = document.getElementById("secondModal");
+let thirdModal = document.getElementById("thirdModal");
+let fourthModal = document.getElementById("fourthModal");
+let fifthModal = document.getElementById("fifthModal");
+
+let firstBtn = document.getElementById("firstContent");
+let secondBtn = document.getElementById("secondContent");
+let thirdBtn = document.getElementById("thirdContent");
+let fourthBtn = document.getElementById("fourthContent");
+let fifthBtn = document.getElementById("fifthContent");
+
+let span = document.getElementsByClassName("close")[0];
+
+firstBtn = function() {
+  firstModal.style.display = "block";
+};
+secondBtn = function() {
+  secondModal.style.display = "block";
+};
+thirdBtn = function() {
+  thirdModal.style.display = "block";
+};
+fourthBtn = function() {
+  fourthModal.style.display = "block";
+};
+fifthBtn = function() {
+  fifthModal.style.display = "block";
+};
+
+span.onclick = function() {
+ firstModal.style.display = "none";
+ secondModal.style.display = "none";
+ thirdModal.style.display = "none";
+ fourthModal.style.display = "none";
+ fifthModal.style.display = "none";
+};
